5c205a725b163ae353830d210e5c7921d28db67d
Michal Podolak <60146710+myky123@users.noreply.github.com>
2024-05-23 09:10:42 +0100

    Add bokstore project

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
+// TODO
+// 1. Create modals with the projects description and visit website option
+// 2. On click on the website, open the project in a new tab
+
 const projectContainer = document.querySelector(".projects-container");
 const projectTemplate = document.querySelector(".project-template");
 const technologiesContainer = document.querySelector(".technologies-grid");
 const technologyTemplate = document.querySelector(".technology-template");
+
+// Open modal
+window.addEventListener("click", (e) => {
+    const projectCard = e.target.closest(".project-card");
+
+    if (projectCard) {
+        const modalOverlay = document.querySelector(".modal-overlay");
+        const modalContainer = document.querySelector(".modal-container");
+        modalOverlay.classList.add("active");
+        modalContainer.classList.add("active");
+
+        const modalProjectDetails = projectCard.querySelector(
+            ".modal-project-details"
+        );
+        const modalProjectName = projectCard.querySelector(
+            ".modal-project-title"
+        );
+
+        // modalProjectDetails.textContent = projectCard.dataset.details;
+    }
+});
+
+// Close modal
+window.addEventListener("click", (e) => {
+    const modalOverlay = document.querySelector(".modal-overlay");
+    const modalContainer = document.querySelector(".modal-container");
+    const closeModal =
+        e.target.closest(".modal-close") || e.target.matches(".modal-overlay");
+
+    if (closeModal) {
+        modalOverlay.classList.remove("active");
+        modalContainer.classList.remove("active");
+    }
+});
 
 async function getData(
     jsonFile,
@@ -42,6 +80,8 @@ function render(
         const templateCopy = template.content.cloneNode(true);
         const itemElement = templateCopy.querySelector(elContainer);
         itemElement.dataset.id = item.id;
+        itemElement.dataset.link = item.url;
+        itemElement.href = item.url;
 
         const itemName = itemElement.querySelector(elName);
         const itemImage = itemElement.querySelector(elImg);
@@ -62,7 +102,7 @@ function render(
 function setCurrentYear() {
     const currentYearElemennt = document.querySelector(".current-year");
 
-    currentYearElemennt.textContent = new Date().getFullYear();
+    // currentYearElemennt.textContent = new Date().getFullYear();
 }
 
 const getProjectsData = getData(
@@ -70,7 +110,7 @@ const getProjectsData = getData(
     render,
     projectTemplate,
     projectContainer,
-    ".project",
+    ".project-card",
     ".project-name",
     ".project-img",
     ".project-details"
@@ -84,7 +124,6 @@ const getTechnologiesData = getData(
     ".technology",
     ".technology-name",
     ".technology-icon",
-
     null
 );
 
